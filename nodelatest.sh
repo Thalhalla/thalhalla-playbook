409c13b50e3d104d16f1a876cdbfb73f3d50257d
Josh Cox <josh@webhosting.coop>
2017-02-17 17:38:12 -0600

    chmods

--- nodelatest.sh
+++ nodelatest.sh
@@ -2,7 +2,7 @@
 
 # install nvm
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
-export NVM_DIR="/home/thoth/.nvm"
+export NVM_DIR="/home/`whoami`/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 
 nvm install lts/boron
