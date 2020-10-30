167e78980adc88ddc5ec17fa13dcbbe30eadc196
Josh Cox <josh@webhosting.coop>
2020-10-30 10:43:27 -0500

    autokey

--- bauerbillExtras.sh
+++ bauerbillExtras.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 TMP=$(mktemp -d)
-#yaourt -S \
-#    glipper \
-#    dissenter-browser
-#    byobu \
-#    autokey-py3 \
-#    cmst \
 for i in $(cat bauerbill_list); do ./bauerbill_installer $i; done
 
 rm -Rf $TMP
