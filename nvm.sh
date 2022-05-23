9c2a5d54503694c9dcbb39f07ec435ffbe100f82
Josh Cox <josh@webhosting.coop>
2022-05-22 19:36:16 -0500

    dmidecode

--- nvm.sh
+++ nvm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 . lib/loader.bash
-release_target=dubnium
+release_target=fermium
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
