c053d3964c6e4bc88930838f5ce42ae67b388740
Joshua Edward McLaughlin Cox <josh@webhosting.coop>
2025-11-23 00:24:54 -0600

    jcs

--- lib/jcx_utils.bash
+++ lib/jcx_utils.bash
@@ -7,22 +7,7 @@ else
 fi
 
 install_jcx_utils () {
-  # VV
-  curl https://raw.githubusercontent.com/joshuacox/vv/master/bootstrapvv.sh |sudo bash
-  # Roustabout
-  curl https://raw.githubusercontent.com/joshuacox/roustabout/master/bootstraproustabout.sh |sudo bash
-  # bomsaway
-  curl https://raw.githubusercontent.com/joshuacox/bomsaway/master/bootstrapbomsaway.sh |sudo bash
-  # SSShutdown
-  curl -sL https://git.io/ssshutdown | bash
-  # Swappy
-  curl https://raw.githubusercontent.com/joshuacox/swappy/master/bootstrap | bash
-  # Passgen
-  curl https://raw.githubusercontent.com/joshuacox/passgen/master/bootstrappassgen.sh | sh
-  # wg-init
-  curl https://raw.githubusercontent.com/joshuacox/wg-init/main/bootstrap | sh
-  # Clip
-  curl -sL https://git.io/clipinstall | bash
+  curl -sL https://raw.githubusercontent.com/joshuacox/jcs/refs/heads/main/bootstrap.sh | bash
 }
 
 puller () {
