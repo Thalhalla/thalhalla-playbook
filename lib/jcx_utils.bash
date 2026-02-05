3226ca536fbe116e49e3346e75932fc914afe3e8
Joshua Edward McLaughlin Cox <josh@webhosting.coop>
2026-02-05 07:50:52 -0600

    stow
    Signed-off-by: Joshua Edward McLaughlin Cox <josh@webhosting.coop>

--- lib/jcx_utils.bash
+++ lib/jcx_utils.bash
@@ -44,16 +44,20 @@ adder () {
   if [[ $(grep -P "^${TARGET}$" pacman_list) == "$TARGET" ]]; then
     echo 'Already added'
   else
-    export DID_SOMETHING=1
     check_hooks
     #sudo powerpill -S --noconfirm $TARGET
     sudo $PACMAN -S --noconfirm $TARGET
-    if [[ ! $? -eq 0 ]]; then 
-      auradder $TARGET
+    if [[ $? -eq 0 ]]; then 
+      export PAC_DID_SOMETHING=1
+    elif [[ ! $? -eq 0 ]]; then 
+      aur_adder $TARGET
     fi
     check_outhooks
     check_pull
-    echo "$TARGET" >> ./pacman_list
+    if [[ $PAC_DID_SOMETHING == 1 ]]; then
+      echo "$TARGET" >> ./pacman_list
+      sort_push
+    fi
   fi
 }
 
@@ -65,16 +69,31 @@ aur_adder () {
   if [[ $(grep -P "^${TARGET}$" aur_list) == "$TARGET" ]]; then
     echo 'Already added'
   else
-    export DID_SOMETHING=1
     check_hooks
     cd $THIS_CWD
-    aur_installer $TARGET
+    aur_installer $TARGET | tee ${THIS_AUR_TMP}
+    grep -P "No AUR package found for ${TARGET}" ${THIS_AUR_TMP} 
+    if [[ $? -eq 0 ]]; then
+      echo 'error: target not found'
+      exit 1
+    else
+      export AUR_DID_SOMETHING=1
+    fi
     cd $THIS_CWD
     check_outhooks
     cd $PWD
     check_pull
-    echo "$TARGET" >> ./aur_list
+    if [[ $AUR_DID_SOMETHING == 1 ]]; then
+      echo "$TARGET" >> ./aur_list
+      sort_push
+    fi
   fi
+}
+
+sort_push () {
+  ./sorter
+  git commit -s -S -am "$TARGET"
+  git push
 }
 
 aur_installer () {
@@ -82,15 +101,11 @@ aur_installer () {
   TMP=$(mktemp -d)
   set -eux
   cd $TMP
-  yay -S $TARGET
-  cd build
-  if [[ -f ./download.sh ]]; then 
-    yes M | ./download.sh
-  fi
-  if [[ -f ./build.sh ]]; then 
-    yes y | ./build.sh
-  fi
-  if [[ -f ./clean.sh ]]; then 
-    ./clean.sh
-  fi
+  yay -S \
+    --answerclean All \
+    --answerdiff All \
+    --answeredit All \
+    --answerupgrade Repo \
+    --noconfirm \
+    $TARGET 
 }
